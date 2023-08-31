4294a30b86cdcf87b7ee6fe5ab97497fa19d928b
JRSiwiecki <JRSiwiecki@gmail.com>
2023-08-30 19:20:24 -0700

    Update buttons to pop-up Snackbars instead of Toasts

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,18 +20,19 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
+            Snackbar.make(
+                view,
                 R.string.correct_toast,
-                Toast.LENGTH_SHORT
+                3000
             ).show()
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
+
+            Snackbar.make(
+                view,
                 R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
+                3000
             ).show()
         }
 
