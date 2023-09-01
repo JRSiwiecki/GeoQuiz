0d1ee7c5bb36740f145ac31653f6a0cb41fabd75
Joseph <46443751+JRSiwiecki@users.noreply.github.com>
2023-09-01 16:46:59 -0700

    Merge Chapter 2 Work (#3)
    * Add question text, true & false buttons, and string resources
    
    * Update buttons to be borderless
    
    * Add IDs to buttons, add correct and incorrect text, add Toast popups for True and False buttons
    
    * Update buttons to pop-up Snackbars instead of Toasts
    
    * Remove unused import
    
    * Add next button, move question strings to be more specific, add proper answer checking, add question cycling, remove unused import
    
    * Add icons for true, false, and next buttons
    
    * Add previous button, refactor how questions are updated

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -3,37 +3,89 @@ package com.bignerdranch.android.geoquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
+
+    private enum class QuestionDirection {
+        PREVIOUS,
+        NEXT
+    }
+
+    private val questionBank = listOf(
+        Question(R.string.question_mongolia, true),
+        Question(R.string.question_brazil, true),
+        Question(R.string.question_bermuda_triangle, false),
+        Question(R.string.question_largest_island, false),
+        Question(R.string.question_mount_rushmore, true)
+    )
+
+    private var currentQuestionIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton.setOnClickListener { view: View ->
-            Snackbar.make(
-                view,
-                R.string.correct_toast,
-                3000
-            ).show()
+        binding.questionTextView.setOnClickListener { view: View ->
+            updateQuestionIndexAndQuestion(QuestionDirection.NEXT)
         }
 
-        falseButton.setOnClickListener { view: View ->
+        binding.trueButton.setOnClickListener { view: View ->
 
-            Snackbar.make(
-                view,
-                R.string.incorrect_toast,
-                3000
-            ).show()
+            checkAnswer(true, view)
         }
 
+        binding.falseButton.setOnClickListener { view: View ->
+
+            checkAnswer(false, view)
+        }
+
+        binding.nextButton.setOnClickListener { view: View ->
+
+            updateQuestionIndexAndQuestion(QuestionDirection.NEXT)
+
+        }
+
+        binding.previousButton.setOnClickListener { view: View ->
+            updateQuestionIndexAndQuestion(QuestionDirection.PREVIOUS)
+        }
+
+        updateQuestion()
+    }
+
+
+    private fun updateQuestionIndexAndQuestion(questionDirection: QuestionDirection) {
+        currentQuestionIndex = if (questionDirection == QuestionDirection.PREVIOUS) {
+            (currentQuestionIndex - 1 + questionBank.size) % questionBank.size
+        } else {
+            (currentQuestionIndex + 1) % questionBank.size
+        }
+
+        updateQuestion()
+    }
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentQuestionIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean, view: View) {
+        val correctAnswer = questionBank[currentQuestionIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+
+        Snackbar.make(
+            view,
+            messageResId,
+            3000
+        ).show()
     }
 }
