48e8b6c5985401d6c9d607e34d652b1cfb41623f
Joseph <46443751+JRSiwiecki@users.noreply.github.com>
2023-09-03 00:20:27 -0700

    Merge Chapter 3 Work (#4)
    * Add logs for each state change
    
    * Add UserAnswerState enum to track if question has been answered, track user's Answer to each question, update buttons if a question has been answered yet
    
    * Display user score at end of quiz, refactor function names

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,10 +2,13 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
+
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -16,17 +19,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val questionBank = listOf(
-        Question(R.string.question_mongolia, true),
-        Question(R.string.question_brazil, true),
-        Question(R.string.question_bermuda_triangle, false),
-        Question(R.string.question_largest_island, false),
-        Question(R.string.question_mount_rushmore, true)
+        Question(R.string.question_mongolia, correctAnswer = true, userAnswer = false, answerState = UserAnswerState.UNANSWERED),
+        Question(R.string.question_brazil, correctAnswer = true, userAnswer = false, answerState = UserAnswerState.UNANSWERED),
+        Question(R.string.question_bermuda_triangle, correctAnswer = false, userAnswer = false, answerState = UserAnswerState.UNANSWERED),
+        Question(R.string.question_largest_island, correctAnswer = false, userAnswer = false, answerState = UserAnswerState.UNANSWERED),
+        Question(R.string.question_mount_rushmore, correctAnswer = true, userAnswer = false, answerState = UserAnswerState.UNANSWERED)
     )
 
     private var currentQuestionIndex = 0
+    private var numberOfQuestionsAnswered = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "onCreate(Bundle?) called")
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -58,6 +64,31 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart(Bundle?) called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume(Bundle?) called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause(Bundle?) called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop(Bundle?) called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy(Bundle?) called")
+    }
+
 
     private fun updateQuestionIndexAndQuestion(questionDirection: QuestionDirection) {
         currentQuestionIndex = if (questionDirection == QuestionDirection.PREVIOUS) {
@@ -71,10 +102,41 @@ class MainActivity : AppCompatActivity() {
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentQuestionIndex].textResId
         binding.questionTextView.setText(questionTextResId)
+
+        updateButtons()
+    }
+
+    private fun updateButtons() {
+        val currentQuestion = getCurrentQuestion()
+
+        if (currentQuestion.answerState == UserAnswerState.ANSWERED) {
+            disableAnswerButtons()
+        } else {
+            enableAnswerButtons()
+        }
+    }
+    private fun getCurrentQuestion(): Question {
+        return questionBank[currentQuestionIndex]
+    }
+
+    private fun disableAnswerButtons() {
+        binding.trueButton.isEnabled = false
+        binding.trueButton.isClickable = false
+        binding.falseButton.isEnabled = false
+        binding.falseButton.isClickable = false
+    }
+
+    private fun enableAnswerButtons() {
+        binding.trueButton.isEnabled = true
+        binding.trueButton.isClickable = true
+        binding.falseButton.isEnabled = true
+        binding.falseButton.isClickable = true
     }
 
     private fun checkAnswer(userAnswer: Boolean, view: View) {
-        val correctAnswer = questionBank[currentQuestionIndex].answer
+        val currentQuestion = getCurrentQuestion()
+
+        val correctAnswer = questionBank[currentQuestionIndex].correctAnswer
 
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
@@ -82,10 +144,42 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect_toast
         }
 
+        currentQuestion.userAnswer = userAnswer
+        currentQuestion.answerState = UserAnswerState.ANSWERED
+        updateButtons()
+
         Snackbar.make(
             view,
             messageResId,
-            3000
+            Snackbar.LENGTH_SHORT
         ).show()
+
+        numberOfQuestionsAnswered += 1
+
+        if (numberOfQuestionsAnswered == questionBank.size) {
+            val userScore = calculateScore()
+
+            Snackbar.make(
+                view,
+                "Congrats! Your score is $userScore%",
+                Snackbar.LENGTH_INDEFINITE
+            ).show()
+
+        }
+    }
+
+    private fun calculateScore(): Double {
+        var userQuizScore = 0.0
+
+        for (question in questionBank) {
+            val userAnswer = question.userAnswer
+            val questionAnswer = question.correctAnswer
+
+            if (userAnswer == questionAnswer) {
+                userQuizScore += 1.0
+            }
+        }
+
+        return (userQuizScore / questionBank.size) * 100.0
     }
 }
--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
@@ -2,6 +2,10 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.annotation.StringRes
 
-data class Question(@StringRes val textResId: Int, val answer: Boolean) {
+enum class UserAnswerState {
+    UNANSWERED,
+    ANSWERED
+}
+data class Question(@StringRes val textResId: Int, val correctAnswer: Boolean, var userAnswer: Boolean, var answerState: UserAnswerState) {
 
 }
