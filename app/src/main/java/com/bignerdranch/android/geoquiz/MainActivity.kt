01564abe90d2d4a5c3d5b4ac2cc1ea71604d00ce
JRSiwiecki <JRSiwiecki@gmail.com>
2023-09-08 18:13:50 -0700

    Count question as wrong when user cheats

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -154,7 +154,12 @@ class MainActivity : AppCompatActivity() {
             else -> R.string.incorrect_toast
         }
 
-        currentQuestion.userAnswer = userAnswer
+        // Set user answer to opposite of correct answer when they cheat!
+        currentQuestion.userAnswer = when {
+            quizViewModel.isCheater -> !(currentQuestion.correctAnswer)
+            else -> userAnswer
+        }
+
         currentQuestion.answerState = UserAnswerState.ANSWERED
         updateButtons()
 
